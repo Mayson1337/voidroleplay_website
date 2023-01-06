66bc931dfc63818eca94d68f7c19058e36c648f2
Mayson1337 <99101632+Mayson1337@users.noreply.github.com>
2023-01-06 03:15:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function openHome() {
+    window.open("index.html")
+}
+
+function openAbout() {
+    window.open("about.html")
+}
+
+function openRules() {
+    window.open("rules.html")
+}
+
+function openDonation() {
+    window.open("donation.html")
+}
